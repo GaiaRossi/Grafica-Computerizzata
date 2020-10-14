35e7d81db0ec9ff905546f35beb86ef09812bbd0
GaiaRossi <gaia.rossi@student.unife.it>
2020-10-14 11:34:05 +0200

    Grafico da finestra

--- esercizioLezione4.c
+++ esercizioLezione4.c
@@ -10,12 +10,25 @@
 // Number of elements in the circular buffer
 #define NELS    10
 
+#define MAXPUNTI 10
+#define DIM 1000.0
+#define BORDERSIZE 10.0
+
+int hasColore = 1;
+
+typedef struct{
+    float x;
+    float y;
+} PuntoGrafico;
+
+PuntoGrafico puntiNoColore[MAXPUNTI];
+PuntoGrafico puntiColore[MAXPUNTI];
 // circular buffer used to compute frame rate
 float circularBuffer[NELS];
 int firstInd = 0, nEls = 0;
 
-//massimo numero che il mio pc sopporta
 int curLines = 1;
+int j = 0;
 
 // function to get the number of elapsed ticks
 uint32_t getTick()
@@ -82,41 +95,109 @@ void computeAndShowFrameRate(void)
         // sum elements in circular buffer
         sumFPS = sumCircularBuffer();
         snprintf(windowTitle, 100, "FPS = %6.2f", sumFPS/nEls);
-
-        printf("%d\t%f\n", curLines, sumFPS/nEls);
-
+        
         // update window title
         glutSetWindowTitle(windowTitle);
+
+        //salvataggio coordinate punti grafico
+
+        if(j < MAXPUNTI){
+            if(hasColore == 1){
+                puntiNoColore[j].x = curLines;
+                puntiNoColore[j].y = sumFPS/(nEls);
+                
+                printf("linee disegnate: %f\t fps: %f\n", puntiNoColore[j].x, sumFPS/nEls);
+                j++;
+            }
+            else{
+                puntiColore[j].x = curLines;
+                puntiColore[j].y = sumFPS/(nEls);
+                
+                printf("linee disegnate: %f\t fps: %f\n", puntiColore[j].x, sumFPS/nEls);
+                j++;
+            }
+        }
     }
 }
 
 void display(){
     int i;
 
-    computeAndShowFrameRate();
+    if(j < MAXPUNTI){
+        computeAndShowFrameRate();
 
-    glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-    glBegin(GL_LINES);
-    
-    for(i = 0; i < curLines; i++){
-        glColor3f((float) rand()/RAND_MAX, (float) rand()/RAND_MAX, (float) rand()/RAND_MAX);
-        glVertex2f((float) rand()/RAND_MAX, (float) rand()/RAND_MAX);
-        glColor3f((float) rand()/RAND_MAX, (float) rand()/RAND_MAX, (float) rand()/RAND_MAX);
-        glVertex2f((float) rand()/RAND_MAX, (float) rand()/RAND_MAX);
+        glBegin(GL_LINES);
+        
+        for(i = 0; i < curLines; i++){
+            glColor3f((float) rand()/RAND_MAX, (float) rand()/RAND_MAX, (float) rand()/RAND_MAX);
+            glVertex2f((float) rand()/RAND_MAX * DIM, (float) rand()/RAND_MAX * DIM);
+            if(hasColore == 0){
+                glColor3f((float) rand()/RAND_MAX, (float) rand()/RAND_MAX, (float) rand()/RAND_MAX);
+            }
+            glVertex2f((float) rand()/RAND_MAX * DIM, (float) rand()/RAND_MAX * DIM);
+        }
+
+        curLines += 10;
+
+        glEnd();
+
+        glFlush();
+        /*
+        glutpostredisplay dice alla opengl che la finestra corrente
+        necessita di essere nuovamente mostrata, quindi verra richiamata
+        la funzione display
+        */
+        glutPostRedisplay();
     }
+    else{
+        if(hasColore == 1){
+            j = 0;
+            hasColore = 0;
+            curLines = 1;
+            glutPostRedisplay();
+        }
+        else{
+            printf("Test ended\n");
+            glClear(GL_COLOR_BUFFER_BIT);
 
-    curLines += 10;
+            glBegin(GL_LINES);
 
-    glEnd();
+                glColor3d(1.0, 1.0, 1.0);
+                //asse y
+                glVertex2f(0.0, 0.0);
+                glVertex2f(0.0, DIM);
+                //asse x
+                glVertex2f(0.0, 0.0);
+                glVertex2f(DIM, 0.0);
+            glEnd();
 
-    glFlush();
-    /*
-    glutpostredisplay dice alla opengl che la finestra corrente
-    necessita di essere nuovamente mostrata, quindi verra richiamata
-    la funzione display
-    */
-    glutPostRedisplay();
+            glBegin(GL_LINE_STRIP);
+
+                glColor3f(1.0, 0.0, 0.0);
+                for(j = 0; j < MAXPUNTI; j++){
+                    printf("x: %f\ty: %f\n", (float) puntiNoColore[j].x/DIM * 10, puntiNoColore[j].y);
+                    glVertex2f((float) puntiNoColore[j].x/DIM * 10, puntiNoColore[j].y);
+                }
+
+            glEnd();
+
+            printf("\n");
+            
+            glBegin(GL_LINE_STRIP);
+
+                glColor3f(0.0, 0.0, 1.0);
+                for(j = 0; j < MAXPUNTI; j++){
+                    printf("x: %f\ty: %f\n", (float) puntiColore[j].x/DIM * 10, puntiColore[j].y);
+                    glVertex2f((float) puntiColore[j].x/DIM * 10, puntiColore[j].y);
+                }
+
+            glEnd();
+
+            glFlush();
+        }
+    }
 }
 
 void init(){
@@ -127,17 +208,19 @@ void init(){
 
     glMatrixMode(GL_PROJECTION);
 
-    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    glOrtho(-BORDERSIZE, DIM + BORDERSIZE, -BORDERSIZE, DIM + BORDERSIZE, -1.0, 1.0);
 
 }
 
 int main(int argc, char** argv){
 
+    printf("Testing...\n");
+
     glutInit(&argc, argv);
 
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 
-    glutInitWindowSize(400, 400);
+    glutInitWindowSize(600, 400);
 
     glutInitWindowPosition(100, 100);
 
