ad4eb42aaa27ce1fb96e7ae31d8b02e5e873e264
GaiaRossi <gaia.rossi@student.unife.it>
2021-01-09 12:14:31 +0100

    assignment 3 con vao fatto

--- assignement3/assignment3.c
+++ assignement3/assignment3.c
@@ -2,23 +2,87 @@
 #include <GL/glut.h>
 #include <stdio.h>
 
+#include "cubo.h"
+
+unsigned int vao[1];
+unsigned int buffers[2];
+
 void display(){
+    GLint indFace;
 
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glutSwapBuffers();
+    glPushMatrix();
+    glTranslatef(10.0, 10.0, -3.0);
+    glRotatef(30, 0.0, 1.0, 0.0);
+    glRotatef(-20, 1.0, 0.0, 0.0);
+
+    glBindVertexArray(vao[0]);
+
+    for(indFace = 0; indFace < NFACCE; indFace++){
+        //printf("Disegno faccia n: %d\n", indFace);
+        glDrawElements(GL_TRIANGLE_FAN,                               // mode
+                    NVERTICI,                                         // count
+                    GL_UNSIGNED_INT,                                  // type
+                    (GLvoid *)(indFace * NVERTICI * sizeof(GLuint))); // indices
+    }
+
+    glPopMatrix();
+
+    glFinish();
+    glutPostRedisplay();
 
 }
 
 void init(){
 
+    GLenum glErr;
+    if ((glErr=glGetError()) != 0) {
+            printf("Errore = %d \n", glErr);
+            exit(-1);
+        }
+
     glClearColor(0.0, 0.0, 0.0, 0.0);
 
+    glMatrixMode(GL_PROJECTION);
+    //glFrustum(0.0, 20.0, 0.0, 20.0, 2.0, 20.0);
+    glOrtho(0.0, 20.0, 0.0, 20.0, 0.0, 50.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+    glEnable(GL_DEPTH_TEST);
+    glClear(GL_DEPTH_BUFFER_BIT);
+    glDepthFunc(GL_LESS);
+
+    glDisable(GL_CULL_FACE);
+
+    /* inizializzazione vao */
+    glGenVertexArrays(1, vao);
+
+    glBindVertexArray(vao[0]);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+
+    glGenBuffers(2, buffers);
+
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) + sizeof(colors), NULL, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+    glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices), sizeof(colors), colors);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[1]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexPointer(3, GL_FLOAT, 0, 0);
+    glColorPointer(3, GL_FLOAT, 0, (GLvoid *)(sizeof(vertices)));
 }
 
 int main(int argc, char** argv){
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitDisplayMode(GLUT_SINGLE| GLUT_RGB | GLUT_DEPTH);
 
     glutInitWindowSize(600, 400);
     glutInitWindowPosition(200, 700);
